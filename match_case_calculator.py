42d8fbde2c4370c5a5e54995658a35936e4b15cc
Ann-Kiende <annkiende29@gmail.com>
2025-09-21 23:13:47 +0300

    Add match_case_calculator.py

---
+++ match_case_calculator.py
@@ -0,0 +1,25 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case "+":
+        addition = num1 + num2
+        print("The result is "+addition)
+
+    case "-":
+        subtract = num1 - num2
+        print("The result is "+subtract)
+
+    case "*":
+        multiply = num1 * num2
+        print("The result is "+multiply)
+    
+    case "/":
+        divide = num1 / num2
+        if num1 or num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            print("The result is "+divide)
+
